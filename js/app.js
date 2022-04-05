ae15fba8cba9e683756e33420854cf647f84448f
Marcos-Castillo <marcos469204@gmail.com>
2022-04-05 01:20:04 -0300

    js
    comenzando js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+
+let card = ` <div class="card">
+<img class="cardImg" src="${/img/card/cardStarWars/01.jpg}" alt="">
+<p class="cardTitulo">${producto XYZ}</p>
+<p class="cardPrecio">${$60}</p>
+<a class="cardLink" href="${#}">Ver producto</a>
+</div>` ;
